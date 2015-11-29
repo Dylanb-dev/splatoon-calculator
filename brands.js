4a61975541da8686914c0c980df6aa5e92985006
Dylan <20769605@student.uwa.edu.au>
2015-11-29 11:05:52 +0800

    up to date with website

---
+++ brands.js
@@ -0,0 +1,74 @@
+angular.module('splatoonApp').brands = function ($scope) {
+
+	$scope.brands = [{
+		'name' : 'amiibo',
+		'common' : '',
+		'rare' : '',
+	}, {
+		'name' : 'Cuttlegear',
+		'common' : '',
+		'rare' : '',
+	}, {
+		'name' : 'Famitsu',
+		'common' : '',
+		'rare' : '',
+	}, {
+		'name' : 'Firefin',
+		'common' : 'assets/images/Ability_Sub_Ink_Saver.png',
+		'rare' : 'assets/images/Ability_Ink_Recovery_Up.png',
+	}, {
+		'name' : 'Forge',
+		'common' : 'assets/images/Ability_Special_Duration_Up.png',
+		'rare' : 'assets/images/Ability_Sub_Ink_Saver.png',
+	}, {
+		'name' : 'Inkline',
+		'common' : 'assets/images/Ability_Defense_Up.png',
+		'rare' : 'assets/images/Ability_Damage_Up.png',
+	}, {
+		'name' : 'KOG',
+		'common' : '',
+		'rare' : '',
+	}, {
+		'name' : 'Krak-On',
+		'common' : 'assets/images/Ability_Swim_Speed_Up.png',
+		'rare' : 'assets/images/Ability_Defense_Up.png',
+	}, {
+		'name' : 'Rockenberg',
+		'common' : 'assets/images/Ability_Run_Speed_Up.png',
+		'rare' : 'assets/images/Ability_Swim_Speed_Up.png',
+	}, {
+		'name' : 'Skalop',
+		'common' : 'assets/images/Ability_Quick_Respawn.png',
+		'rare' : 'assets/images/Ability_Special_Saver.png',
+	}, {
+		'name' : 'Splash Mob',
+		'common' : 'assets/images/Ability_Main_Ink_Saver.png',
+		'rare' : 'assets/images/Ability_Run_Speed_Up.png',
+	}, {
+		'name' : 'SquidForce',
+		'common' : 'assets/images/Ability_Damage_Up.png',
+		'rare' : 'assets/images/Ability_Main_Ink_Saver.png',
+	}, {
+		'name' : 'Takoroka',
+		'common' : 'assets/images/Ability_Special_Charge_Up.png',
+		'rare' : 'assets/images/Ability_Special_Duration_Up.png',
+	}, {
+		'name' : 'Tentatek',
+		'common' : 'assets/images/Ability_Ink_Recovery_Up.png',
+		'rare' : 'assets/images/Ability_Quick_Super_Jump.png',
+	}, {
+		'name' : 'The SQUID GIRL',
+		'common' : '',
+		'rare' : '',
+	}, {
+		'name' : 'Zekko',
+		'common' : 'assets/images/Ability_Special_Saver.png',
+		'rare' : 'assets/images/Ability_Special_Charge_Up.png',
+	}, {
+		'name' : 'Zink',
+		'common' : 'assets/images/Ability_Quick_Super_Jump.png',
+		'rare' : 'assets/images/Ability_Quick_Respawn.png'
+
+	}];
+
+};
