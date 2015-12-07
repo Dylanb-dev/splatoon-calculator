89bc606b119f5f362e7295c52390b655dc53697a
Dylan <20769605@student.uwa.edu.au>
2015-12-07 16:38:02 +0800

    merge with master

--- brands.js
+++ brands.js
@@ -1,73 +1,56 @@
 angular.module('splatoonApp').brands = function ($scope) {
 
 	$scope.brands = [{
-		'name' : 'amiibo',
-		'common' : '',
-		'rare' : '',
+		name : 'amiibo',
+		rolls : ''
 	}, {
-		'name' : 'Cuttlegear',
-		'common' : '',
-		'rare' : '',
+		name : 'Cuttlegear',
+		rolls : ''
 	}, {
-		'name' : 'Famitsu',
-		'common' : '',
-		'rare' : '',
+		name : 'Famitsu',
+		rolls : ''
 	}, {
-		'name' : 'Firefin',
-		'common' : 'assets/images/Ability_Sub_Ink_Saver.png',
-		'rare' : 'assets/images/Ability_Ink_Recovery_Up.png',
+		name : 'Firefin',
+		rolls : ''
 	}, {
-		'name' : 'Forge',
-		'common' : 'assets/images/Ability_Special_Duration_Up.png',
-		'rare' : 'assets/images/Ability_Sub_Ink_Saver.png',
+		name : 'Forge',
+		rolls : ''
 	}, {
-		'name' : 'Inkline',
-		'common' : 'assets/images/Ability_Defense_Up.png',
-		'rare' : 'assets/images/Ability_Damage_Up.png',
+		name : 'Inkline',
+		rolls : ''
 	}, {
-		'name' : 'KOG',
-		'common' : '',
-		'rare' : '',
+		name : 'KOG',
+		rolls : ''
 	}, {
-		'name' : 'Krak-On',
-		'common' : 'assets/images/Ability_Swim_Speed_Up.png',
-		'rare' : 'assets/images/Ability_Defense_Up.png',
+		name : 'Krak-On',
+		rolls : ''
 	}, {
-		'name' : 'Rockenberg',
-		'common' : 'assets/images/Ability_Run_Speed_Up.png',
-		'rare' : 'assets/images/Ability_Swim_Speed_Up.png',
+		name : 'Rockenberg',
+		rolls : ''
 	}, {
-		'name' : 'Skalop',
-		'common' : 'assets/images/Ability_Quick_Respawn.png',
-		'rare' : 'assets/images/Ability_Special_Saver.png',
+		name : 'Skalop',
+		rolls : ''
 	}, {
-		'name' : 'Splash Mob',
-		'common' : 'assets/images/Ability_Main_Ink_Saver.png',
-		'rare' : 'assets/images/Ability_Run_Speed_Up.png',
+		name : 'Splash Mob',
+		rolls : ''
 	}, {
-		'name' : 'SquidForce',
-		'common' : 'assets/images/Ability_Damage_Up.png',
-		'rare' : 'assets/images/Ability_Main_Ink_Saver.png',
+		name : 'SquidForce',
+		rolls : ''
 	}, {
-		'name' : 'Takoroka',
-		'common' : 'assets/images/Ability_Special_Charge_Up.png',
-		'rare' : 'assets/images/Ability_Special_Duration_Up.png',
+		name : 'Takoroka',
+		rolls : ''
 	}, {
-		'name' : 'Tentatek',
-		'common' : 'assets/images/Ability_Ink_Recovery_Up.png',
-		'rare' : 'assets/images/Ability_Quick_Super_Jump.png',
+		name : 'Tentatek',
+		rolls : {"Ink Recovery Up":"1/3.3","Quick Super Jump":"1/33"}
 	}, {
-		'name' : 'The SQUID GIRL',
-		'common' : '',
-		'rare' : '',
+		name : 'The SQUID GIRL',
+		rolls : ''
 	}, {
-		'name' : 'Zekko',
-		'common' : 'assets/images/Ability_Special_Saver.png',
-		'rare' : 'assets/images/Ability_Special_Charge_Up.png',
+		name : 'Zekko',
+		rolls : ''
 	}, {
-		'name' : 'Zink',
-		'common' : 'assets/images/Ability_Quick_Super_Jump.png',
-		'rare' : 'assets/images/Ability_Quick_Respawn.png'
+		name : 'Zink',
+		rolls : ''
 
 	}];
 
